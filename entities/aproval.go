97981a6c4ae109c036a2edd089b6f281b5504060
David Motta B <mdavid20@gmail.com>
2023-05-02 20:29:30 -0500

    update

--- entities/aproval.go
+++ entities/aproval.go
@@ -1,13 +1,13 @@
 package entities
 
 type Proyecto struct {
-	IdProyecto    uint   `json:"idProyecto"`
-	IdCliente     uint   `json:"idCliente"`
-	Fecha         string `json:"fecha"`
-	Descripcion   string `json:"descripcion"`
-	Importe       string `json:"importe"`
-	IdEstado      string `json:"idEstado"`
-	IdUsuario     uint   `json:"idUsuario"`
-	FechaCreacion string `json:"fechaCreacion"`
-	Activo        bool   `json:"activo"`
+	IdProyecto    uint    `json:"idProyecto"`
+	IdCliente     uint    `json:"idCliente"`
+	Fecha         string  `json:"fecha"`
+	Descripcion   string  `json:"descripcion"`
+	Importe       float64 `json:"importe"`
+	IdEstado      string  `json:"idEstado"`
+	IdUsuario     uint    `json:"idUsuario"`
+	FechaCreacion string  `json:"fechaCreacion"`
+	Activo        bool    `json:"activo"`
 }
